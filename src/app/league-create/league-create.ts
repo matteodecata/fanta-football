3598f8cc61a7408a3e6582c5e8de36da55b36300
MyriamSpagnuolo <spagnuolomyriam@gmail.com>
2026-09-03 17:56:44 +0200

    crea nuove componenti

---
+++ src/app/league-create/league-create.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {  } from '@angular/common';
+
+@Component({
+  selector: 'app-league-create',
+  imports: [],
+  templateUrl: './league-create.html',
+  styleUrl: './league-create.css',
+})
+export class LeagueCreate {}
