f35eb5a2912a3f399c2542f9c7b32545722e668a
Matteo_DeCata <mecata99@gmail.com>
2026-09-07 12:53:03 +0200

    aggiunto il log out all'account

--- src/app/account/account.ts
+++ src/app/account/account.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Session } from '../core/auth/session';
 import { ChangePassword } from './change-password/change-password';
 import { ChangeUsername } from './change-username/change-username';
 import { DisableAccount } from './disable-account/disable-account';
@@ -9,4 +11,12 @@ import { DisableAccount } from './disable-account/disable-account';
   templateUrl: './account.html',
   styleUrl: './account.css',
 })
-export class Account {}
+export class Account {
+  private readonly session = inject(Session);
+  private readonly router = inject(Router);
+
+  protected logout(): void {
+    this.session.logout();
+    void this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
+}
