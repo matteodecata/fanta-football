d6705d1a1a500d60308748843321b005dfabffe2
Wen Jun Zheng Giacomo <wenjunzheng2001@gmail.com>
2026-09-09 10:28:14 +0200

    aggiunto logica di trade per signals

--- src/app/team-trades/team-trades.service.ts
+++ src/app/team-trades/team-trades.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Observable } from "rxjs";
 import { TeamResponse } from '../dashboard/user-leagues/user-leagues.service';
-import { PlayerResponse } from '../players/players-response';
 import { LeagueDetailResponse } from '../league-detail/league-detail.models';
+import { TeamPlayerResponse } from '../team-detail/team-detail.models';
 import { CreateTradeDto, TeamStandingResponse, TradeDto } from './team-trades.models';
 
 @Service()
@@ -11,8 +11,8 @@ export class TeamTradesService {
   private readonly http = inject(HttpClient);
 
   
-  createTrade(trade: CreateTradeDto){
-      this.http.post<TradeDto>('/api/trades', trade);
+  createTrade(trade: CreateTradeDto): Observable<TradeDto> {
+    return this.http.post<TradeDto>('/api/trades', trade);
   }
 
   getUserTrades(): Observable<TradeDto[]>{
@@ -31,8 +31,8 @@ export class TeamTradesService {
     return this.http.get<TeamStandingResponse[]>("/api/leagues/" + id + "/teams");
   }
 
-  getTeamPlayers(teamId: number): Observable<PlayerResponse[]>{
-    return this.http.get<PlayerResponse[]>(teamId + "/players");
+  getTeamPlayers(teamId: number): Observable<TeamPlayerResponse[]>{
+    return this.http.get<TeamPlayerResponse[]>(`/api/teams/${teamId}/players`);
   }
 
   getUserTeams(): Observable<TeamResponse[]>{
